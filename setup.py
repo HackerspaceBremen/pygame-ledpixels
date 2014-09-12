f5cfa4c81b9e16fb4280188be1023652ed019ebd
Michael Kötter <michael@m-koetter.de>
2014-09-12 20:18:49 +0200

    fix setup dependencies

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(name='pygame-ledpixels',
       version='0.1',
       packages=['led'],
-      requires=['pyserial>=2.6', 'pygame>=1.9.1']
+      requires=['pyserial(>=2.6)', 'pygame(>=1.9.1)']
       )
