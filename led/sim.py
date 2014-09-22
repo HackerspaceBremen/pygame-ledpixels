73b6058e034fee476add3cfcbde9e0dc68df9c64
Michael Kötter <michael@m-koetter.de>
2014-09-22 20:35:20 +0200

    handle display initialization error

--- led/sim.py
+++ led/sim.py
@@ -12,13 +12,21 @@ class SimDisplay(base.Display):
         self._size = size
         self._scale = scale
         windowSize = tuple([scale * x for x in size])
-        self._surface = pygame.display.set_mode(windowSize)
+        try:
+            self._surface = pygame.display.set_mode(windowSize)
+        except pygame.error as e:
+            # fallback
+            print "Display error:" , e.message
+            print "SIM display will not work."
+            self._surface = None
 
     def size(self):
         return self._size
 
     def depth(self):
-        return self._surface.get_bitsize()
+        if self._surface:
+            return self._surface.get_bitsize()
+        else: return 0
 
     def draw_grid(self, surface):
         x0 = y0 = 0
@@ -34,7 +42,8 @@ class SimDisplay(base.Display):
             y0 += self._scale
 
     def update(self, surface):
-        pygame.transform.scale(surface, self._surface.get_size(), self._surface)
-        self.draw_grid(self._surface)
-        pygame.display.update()
+        if self._surface:
+            pygame.transform.scale(surface, self._surface.get_size(), self._surface)
+            self.draw_grid(self._surface)
+            pygame.display.update()
 
